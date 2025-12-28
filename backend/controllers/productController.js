dfb1ef9e0f975bb778b105710f933f7bbb1978ef
airmarh <airmarh@gmail.com>
2025-12-28 02:10:24 +0100

    Initial backend implementation

---
+++ backend/controllers/productController.js
@@ -0,0 +1,68 @@
+import cloudinary from 'cloudinary'
+import productModel from '../models/productModel.js'
+
+export const addProduct = async(req, res) => {
+    try{
+        const {name, price, description, category} = req.body
+        const image = req.file;
+        let imageUrl = ""
+        if(image){
+            let result = await cloudinary.uploader.upload(image.path, {resource_type: 'image'})
+            imageUrl = result.secure_url
+        }else{
+            imageUrl = "https://via.placeholder.com/150"
+        }
+
+        const productRequest = {
+            name, description, category, 
+            price: Number(price), 
+            image: imageUrl,
+            date: Date.now()
+        }
+        
+        const product = new productModel(productRequest)
+        await product.save()
+
+        res.status(201).json({success:true, message: "Product added successfully!"})
+    }catch(error){
+        console.log(error)
+        res.status(500).json({success:false, message: "Cannot add product"})
+    }
+}
+
+export const getAllProducts = async(req, res) => {
+    try{
+        const products = await productModel.find({})
+        res.json({success:true, message: "Products retrieved successfully", products: products})
+    }catch(error){
+        console.log(error)
+        res.status(500).json({success:false, message: "Cannot retrieve products"})
+    }
+}
+
+export const removeProduct = async(req, res) => {
+    try{
+        if(!req.body._id){
+            return res.status(400).json({success:false, message: "Reservation id is required"})
+        }
+        await productModel.findByIdAndDelete(req.body._id);
+        res.json({success:true, message: "Product removed"})
+    }catch(error){
+        console.log(error)
+        res.status(500).json({success:false, message: "Cannot remove product"})
+    }
+    
+}
+
+export const getProduct = async(req, res) => {
+    try{
+        if(!req.body._id){
+            return res.status(400).json({success:false, message: "Reservation id is required"})
+        }
+        const product = await productModel.findById(req.body._id);
+        res.json({success:true, message: "Product retrieved", product: product})
+    }catch(error){
+        console.log(error)
+        res.status(500).json({success:false, message: "Cannot get product information"})
+    }
+}
